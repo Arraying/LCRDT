43290e9d7f7d30cc191b79b76c2a061a4421fee5
samlam111 <sam.horacek@gmail.com>
2024-03-02 12:19:44 +0100

    2PC project set-up

---
+++ twopc/lib/twopc.ex
@@ -0,0 +1,18 @@
+defmodule Twopc do
+  @moduledoc """
+  Documentation for `Twopc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Twopc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
