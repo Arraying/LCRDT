e8dba63cb36b7975addf3ef1f073d46feeddb57a
Paul Hübner <paul.huebner@googlemail.com>
2024-03-05 20:27:37 +0100

    Further 2PC logic.

--- lib/logging.ex
+++ lib/logging.ex
@@ -12,23 +12,47 @@ defmodule LCRDT.Logging do
   """
   @logdir "logs"
 
-  def has_uncommitted(logs) do
+  def peek_change(logs) do
     case logs do
-      [{:change, _, _} | _] -> true
-      _ -> false
+      [{:change, tid, _} | _] ->
+        {:found, tid}
+      _ ->
+        :not_found
     end
   end
 
-  def log_change(logs, tid, body) do
-    [{:change, tid, body} | logs]
+  def find_outcome(logs, tid) do
+    case logs do
+      # Base case: we could not find a committed change corresponding to that transaction.
+      [] ->
+        :not_found
+      # We found one that matches.
+      [{:finalize, outcome, ^tid} | {:change, _, body}] ->
+        # We know that after (so before in terms of time) there will be a change.
+        # This is the change that we are interested in, as this is the last committed change.
+        {:found, outcome, body}
+      # We did not find a match, continue recursively.
+      [_ | next] ->
+        find_outcome(next, tid)
+    end
   end
 
-  def log_commit(logs, tid) do
-    [{:finalize, :commit, tid} | logs]
+  def log_change(name, logs1, tid, body) do
+    logs2 = [{:change, tid, body} | logs1]
+    write(name, logs2)
+    logs2
   end
 
-  def log_abort(logs, tid) do
-    [{:finalize, :abort, tid} | logs]
+  def log_commit(name, logs1, tid) do
+    logs2 = [{:finalize, :commit, tid} | logs1]
+    write(name, logs2)
+    logs2
+  end
+
+  def log_abort(name, logs1, tid) do
+    logs2 = [{:finalize, :abort, tid} | logs1]
+    write(name, logs2)
+    logs2
   end
 
   def read(name) do
--- lib/participant.ex
+++ lib/participant.ex
@@ -17,6 +17,7 @@ defmodule LCRDT.Participant do
       :logs,
       :followers,
       :tid,
+      :body,
       stage: :idle,
       votes: Map.new()
     ]
@@ -44,8 +45,6 @@ defmodule LCRDT.Participant do
       if name != Network.coordinator() do
         # We indicate that we are a follower.
         GenServer.cast(Network.coordinator(), {:new_follower, name})
-        # We also send the recovery signal to the coordinator.
-        GenServer.cast(Network.coordinator(), {:recovery, name})
         []
       else
         [name]
@@ -55,10 +54,19 @@ defmodule LCRDT.Participant do
     # If we have an uncommited change in the log, we need to handle this.
     logs = Logging.read(name)
     # Go through all the changes in the log and apply them.
-    apply_logs(logs, application_pid)
-    if Logging.has_uncommitted(logs) do
-      # We have to re-transmit our vote response.
-      # TODO: Retransmit.
+    apply_logs(name, logs, application_pid)
+    # Check for retransmissions.
+    case Logging.peek_change(logs) do
+      # We have to re-transmit our vote response because we're not sure what happened.
+      # If a decision was made, the coordinator will recognize this is a retransmission and tell us what happened.
+      {:found, tid} ->
+        # We only send actions we actually commit, so this will always be a commit.
+        # We can pretty easily recover from this.
+        GenServer.cast(Network.coordinator(), {:vote_response, tid, name, :commit})
+      # At this point we do not know what we are doing, and recovery is a bit more tricky.
+      :not_found ->
+        # We have to send an explicit recovery signal and let the coordinator take charge in our recovery.
+        GenServer.cast(Network.coordinator(), {:recovery, name})
     end
 
     state = %PState{name: name, application_pid: application_pid, logs: logs, followers: followers}
@@ -88,7 +96,7 @@ defmodule LCRDT.Participant do
       # Regular execution.
       true ->
         # We assign this a new transaction ID.
-        state2 = %{state1 | tid: :erlang.make_ref()}
+        state2 = %{state1 | tid: :erlang.make_ref(), body: body}
         # Now we must tell all followers to prepare.
         Enum.each(state2.followers, fn follower_pid ->
           GenServer.cast(follower_pid, {:prepare_request, state2.tid, body})
@@ -106,70 +114,127 @@ defmodule LCRDT.Participant do
     {vote, state2} = case GenServer.call(state1.application_pid, {:prepare, body}, :infinity) do
       :ok ->
         # If this is okay, we need to add it to our own log.
-        logs = Logging.log_change(state1.logs, tid, body)
+        logs = Logging.log_change(state1.name, state1.logs, tid, body)
         {:ok, %{state1 | logs: logs}}
       :abort ->
         # If this is not okay, then we just need to return the message.
         {:abort, state1}
     end
     # Send the response to the actual coordinator.
-    GenServer.cast(Network.coordinator(), {:vote_response, tid, state2.name, vote, body})
+    GenServer.cast(Network.coordinator(), {:vote_response, tid, state2.name, vote})
     out(state2, "Sent a vote response to coodinator: #{vote}")
     {:noreply, state2}
   end
 
   @impl true
-  def handle_cast({:vote_response, _tid, vote_caster, vote, body}, state1) do
-    # TODO: Check if we are finalized. If so, let the node know of the outcome.
-    # Handle vote response messages.
-    state2 = %{state1 | votes: Map.put(state1.votes, vote_caster, vote)}
-    # We wait for all votes to arrive before we possibly abort.
-    # This is so we can be sure we don't get out of order. I think that could be an edge case.
-    state3 =
-      if map_size(state2.votes) == length(state2.followers) do
-        num_aborts = Enum.count(Map.values(state2.votes), fn x -> x == :abort end)
-        out(state2, "Received #{num_aborts} aborts")
-        # Naturally, we abort if at least one node says to abort.
-        action = if num_aborts == 0, do: :commit, else: :abort
-        # Broadcast the outcome to all followers.
-        Enum.each(state2.followers, fn follower_pid ->
-          GenServer.cast(follower_pid, {:finalize, state2.tid, action, body})
-          out(state2, "Sent a finalize request to follower #{follower_pid}")
-        end)
-        # We are done with this now!
-        # We can reset the state back to blank now.
-        out(state2, "Resetting the state")
-        %{state2 | tid: nil, stage: :idle, votes: Map.new()}
-      else
-        state2
-      end
-    # Last but not least, we update the state.
-    {:noreply, state3}
+  def handle_cast({:vote_response, tid, vote_caster, vote}, state1) do
+    cond do
+      # First we check if we have a transaction going on right now.
+      # If not, it means that this is a process trying to catch up and we can do so.
+      state1.stage == :idle ->
+        # We need to re-send the last outcome to get them up to speed.
+        resend_last_outcome(state1, vote_caster, tid)
+        # We continue.
+        {:noreply, state1}
+      # Next we check if we received a vote for this round.
+      # It could be that a process crashed after committing, and they never got the response.
+      # In the meantime, we started a new round.
+      tid != state1.tid ->
+        # They will re-send this vote, but the TID will be different.
+        # In this case, we need to re-send the last outcome.
+        resend_last_outcome(state1, vote_caster, tid)
+        # We also need to get them up to speed on the decision we are making right now.
+        # Sending it in this order requires FIFO links but we have these in Elixir.
+        resend_last_prepare_request(state1, vote_caster)
+        # We continue.
+        {:noreply, state1}
+      # We have a new vote response here.
+      true ->
+        # Handle vote response messages.
+        state2 = %{state1 | votes: Map.put(state1.votes, vote_caster, vote)}
+        # We wait for all votes to arrive before we possibly abort.
+        # This is so we can be sure we don't get out of order. I think that could be an edge case.
+        state3 = try_action(state2)
+        # Last but not least, we update the state.
+        {:noreply, state3}
+    end
   end
 
   @impl true
   def handle_cast({:finalize, tid, :commit, body}, state) do
     # Send a COMMIT indication.
     GenServer.cast(state.application_pid, {:commit, body})
-    logs = Logging.log_commit(state.logs, tid)
+    logs = Logging.log_commit(state.name, state.logs, tid)
     {:noreply, %{state | logs: logs}}
   end
 
   @impl true
   def handle_cast({:finalize, tid, :abort, body}, state) do
     # Send an ABORT indication.
-    GenServer.cast(state.application_pid, {:commit, body})
-    logs = Logging.log_abort(state.logs, tid)
+    GenServer.cast(state.application_pid, {:abort, body})
+    logs = Logging.log_abort(state.name, state.logs, tid)
     {:noreply, %{state | logs: logs}}
   end
 
-  def handle_cast({:recovery, _recovered_pid}, state) do
-    # TODO: Proper recovery handling.
-    {:noreply, state}
+  def handle_cast({:recovery, recovered_pid}, state1) do
+    # The only thing we need to do here is if prepares have been sent out, we abort.
+    # We have nothing in the log for this transaction, so we either:
+    # 1) Never received this message; or
+    # 2) Received this message and aborted it, hence we did not write to the log.
+    # So, to be safe, we must abort.
+    # The rest of the error handling is implemented elsewhere.
+    if state1.stage == :prepared do
+      # Here we have to hard abort.
+      # We can't send the abort message since we don't have the TID.
+      # But we can manipulate internal state and re-call the function.
+      state2 = %{state1 | votes: Map.put(state1.votes, recovered_pid, :abort)}
+      state3 = try_action(state2)
+      {:noreply, state3}
+    else
+      # Otherwise we ignore it, it will figure itself out.
+      {:noreply, state1}
+    end
   end
 
-  defp apply_logs(_logs, _application_pid) do
-    # TODO: Execute these one by one.
+  defp try_action(state2) do
+    # We can only do something once we have all votes.
+    if map_size(state2.votes) == length(state2.followers) do
+      num_aborts = Enum.count(Map.values(state2.votes), fn x -> x == :abort end)
+      out(state2, "Received #{num_aborts} aborts")
+      # Naturally, we abort if at least one node says to abort.
+      action = if num_aborts == 0, do: :commit, else: :abort
+      # Broadcast the outcome to all followers.
+      Enum.each(state2.followers, fn follower_pid ->
+        GenServer.cast(follower_pid, {:finalize, state2.tid, action, state2.body})
+        out(state2, "Sent a finalize request to follower #{follower_pid}")
+      end)
+      # We are done with this now!
+      # We can reset the state back to blank now.
+      out(state2, "Resetting the state")
+      %{state2 | tid: nil, body: nil, stage: :idle, votes: Map.new()}
+    else
+      # Noop.
+      state2
+    end
+  end
+
+  defp apply_logs(_name, _logs, _application_pid) do
+    # We play through in the correct oder.
+    # This can be optimized by only considering commits, but is ignored for brevity.
+    # TODO: Implement this.
+  end
+
+  defp resend_last_outcome(state, syncing_pid, last_tid) do
+    case Logging.find_outcome(state.logs, last_tid) do
+      :not_found ->
+        out(state, "ERROR, could not find last outcome of a crashed process, this is a bug")
+      {:found, outcome, body} ->
+        GenServer.cast(syncing_pid, {:finalize, last_tid, outcome, body})
+    end
+  end
+
+  defp resend_last_prepare_request(state, syncing_pid) do
+    GenServer.cast(syncing_pid, {:prepare_request, state.tid, state.body})
   end
 
   defp out(state, message), do: IO.puts("#{__MODULE__}/#{state.name}: #{message}")
